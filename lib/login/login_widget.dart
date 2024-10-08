a535d1475d2b833cb0cca5d8ed341611881e6572
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-08 01:22:54 +0000

    Updating to latest FlutterFlow output.

--- lib/login/login_widget.dart
+++ lib/login/login_widget.dart
@@ -1,4 +1,5 @@
 import '/auth/firebase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -815,6 +816,28 @@ class _LoginWidgetState extends State<LoginWidget>
                                     ),
                                     createdTime: getCurrentTimestamp,
                                   ));
+
+                              await SendEmailCall.call(
+                                to: _model.signupEmailTextController.text,
+                                subject: 'Welcome to the Todo App',
+                                text:
+                                    'This is an email confirming your account creation.',
+                              );
+
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text(
+                                    'Please check your email for a welcome message.',
+                                    style: TextStyle(
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText,
+                                    ),
+                                  ),
+                                  duration: const Duration(milliseconds: 4000),
+                                  backgroundColor:
+                                      FlutterFlowTheme.of(context).secondary,
+                                ),
+                              );
 
                               context.goNamedAuth(
                                   'onboarding', context.mounted);
